09b8603234e19aceee539e39146d77865222c5e2
rajlakshmi18704 <rajlakshmi18704@gmail.com>
2024-12-26 16:24:26 +0530

    Added clodinary and multer .

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLODINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    const uploadOnClodinary= async (localFilePath)=>{
+        try{
+if(!localFilePath)return null
+//upload the file.
+ const response= await cloudinary.uploader.upload(localFilePath,{
+    resource_type:"auto"
+})
+
+//file has been uploaded successfully.
+console.log("file is uploaded on cloudinary",response.url)
+return response;
+        }
+        catch(error){
+fs.unlinkSync(localFilePath)//remove the locally saved temporary file as operation got failed
+        }
+    }
+    
+  export {uploadOnClodinary}
